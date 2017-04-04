9cfdc1b1af87ca12a5a54800af9eb39fbddbf352
tonyfast <tony.fast@gmail.com>
2017-04-03 20:17:36 -0400

    Add application, exporter, and magic application

--- mdconvert/__init__.py
+++ mdconvert/__init__.py
@@ -1,2 +1,5 @@
-from .mdconvertapp import launch_instance  # noqa
+from .mdconvertapp import launch_instance, metadata_magic  # noqa
 from ._version import __version__  # noqa
+
+from IPython import get_ipython
+get_ipython().register_magic_function(metadata_magic, magic_kind='line_cell', magic_name='metadata')
